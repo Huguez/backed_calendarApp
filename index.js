73d6d7663633cede2e67a80461657d8b311f5303
Huguez <carlos.huguez@gmail.com>
2021-07-20 16:08:44 -0700

    variables de entorno y carpeta publica

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const express = require('express')
+const dotenv = require('dotenv').config();
 
+//  process.env.PORT  === dotenv.parsed.PORT
+
+//crea el servidor 
 const app = express();
+
+// directorio publico
+app.use( express.static( 'public' ) )
+
+
+// app.get('/', ( req, res ) => {
+//     res.json( {
+//         ok:true
+//     } )
+// })
+
+// escuchar peticiones
+app.listen( dotenv.parsed.PORT, () => {
+    console.log( "servidor corriendo!!!" )
+} )
