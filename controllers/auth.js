155da16f249cf3b3dc5135206aacaff2d3448083
Huguez <carlos.huguez@gmail.com>
2021-07-21 00:01:37 -0700

    login usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const { responce } = require('express')
-const { genSaltSync, hashSync } =  require('bcryptjs')
+const { genSaltSync, hashSync, compareSync } =  require('bcryptjs')
 
 const Usuario = require('../models/UsuarioModel')
 
@@ -49,13 +49,43 @@ const renovarToken = ( req, res = responce ) => {
 
 } 
 
-const LoginUsuario = ( req, res = responce ) => {
+const LoginUsuario = async ( req, res = responce ) => {
 
-    return res.status( 202 ).json( {
-        ok: true,
-        user: req.body
-    } );
+    try{
+        const { email, password } = req.body
+        
+        const user = await Usuario.findOne( { email } )
+        
+        if( !user ){
+            return res.status( 404 ).json( {
+                ok: false,
+                msg: "El usuario NO Existe"
+            } );
+        }
 
+        const validPass = compareSync( password, user.password )
+        
+        if( !validPass ){
+            return res.status( 400 ).json( {
+                ok: false,
+                msg: "password incorrecto"
+            } );
+        }
+
+
+
+        return res.status( 202 ).json( {
+            ok: true,
+            user
+        } );
+
+    }catch( error ){
+        console.log( error )
+        return res.status( 500 ).json( {
+            ok: false,
+            msg:"Error del servidor"
+        } );      
+    }
 } 
 
 
